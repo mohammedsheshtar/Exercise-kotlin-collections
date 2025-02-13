e5e16911186201de83bfbb2d78c672a2d153d720
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-13 14:26:50 +0300

    Finished part5: created a list of numbers greater than 15

--- src/Collections.kt
+++ src/Collections.kt
@@ -30,4 +30,10 @@ fun main() {
     carBrands.remove("Kia");
     println("[*] " + carBrands);
 
+    // part 5
+    println("\nPart 5:");
+    val numbers = listOf(10, 5, 20, 15, 25, 30);
+    println("[*] Numbers greater than 15: ${numbers.filter {it > 15}}");
+    println("[*] Sorted List: ${numbers.sorted()}");
+
 }
