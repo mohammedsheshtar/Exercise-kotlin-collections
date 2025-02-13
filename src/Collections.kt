4cd49ab8f7adf973858a8c8da3e90e95801bc045
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-13 13:43:38 +0300

    Finished part2: created a set of numbers

--- src/Collections.kt
+++ src/Collections.kt
@@ -6,4 +6,11 @@ fun main() {
     fruits.remove(fruits[1]);
     println("[*] " + fruits);
 
+    // part 2
+    println("\nPart 2:");
+    val dupNums = mutableSetOf(1, 2, 3, 3, 4, 5, 5);
+    dupNums.add(3);
+    dupNums.add(6);
+    println("[*] " + dupNums);
+
 }
